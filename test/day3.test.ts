18f43c0a5c0a21775dc0d1854de179282fa7efa3
Alex Youngman <alex@Alexs-MacBook-Air.local>
2023-12-09 21:54:30 +0000

    complete day 3 part 1

--- test/day3.test.ts
+++ test/day3.test.ts
@@ -1,4 +1,9 @@
-import { foo } from '../src/day3';
+import {
+  extractValidNumbers,
+  sumActiveNumbers
+} from '../src/day3';
+import day3DataSmall from './data/day3DataSmall';
+import day3Data from './data/day3Data';
 
 const testData = [
   '467..114..',
@@ -13,8 +18,39 @@ const testData = [
   '.664.598..'
 ];
 
-describe('Day 2 getGameNumber', () => {
-  it('gets single digit number from basic string', () => {
-    expect(foo(testData)).toBe(1);
+describe('Day 3 getGameNumber', () => {
+  it('gets single number from small set', () => {
+    const smallTestData = [
+      '467..114..',
+      '...*......',
+    ]
+    expect(extractValidNumbers(smallTestData)).toStrictEqual(['467']);
+  });
+  it('gets array of numbers from moderate set', () => {
+    const expected = [
+      '467', '35', '633',
+      '617', '592', '755',
+      '664', '598'
+    ];
+    expect(extractValidNumbers(testData)).toStrictEqual(expected);
+  });
+});
+
+describe('Day 3 sumActiveNumbers', () => {
+  it('sum of numbers from small set', () => {
+    const smallTestData = [
+      '467..114?.',
+      '...*......',
+    ]
+    expect(sumActiveNumbers(smallTestData)).toStrictEqual(581);
+  });
+  it('sum of numbers from moderate set', () => {
+    expect(sumActiveNumbers(testData)).toStrictEqual(4361);
+  });
+  it('sum of numbers from large set', () => {
+    expect(sumActiveNumbers(day3DataSmall)).toStrictEqual(16587);
+  });
+  it('sum of numbers from huge set', () => {
+    expect(sumActiveNumbers(day3Data)).toStrictEqual(531932);
   });
 });
