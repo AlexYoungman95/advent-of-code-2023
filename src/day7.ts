f3409b7b6d7161bf8690e3ae140d95a953ad9c81
Alex Youngman <alex@Alexs-MacBook-Air.local>
2023-12-17 10:07:54 +0000

    finish day 7

--- src/day7.ts
+++ src/day7.ts
@@ -1,11 +1,112 @@
+const strengthObj = {
+  'A': 12,
+  'K': 11,
+  'Q': 10,
+  'J': 9,
+  'T': 8,
+  '9': 7,
+  '8': 6,
+  '7': 5,
+  '6': 4,
+  '5': 3,
+  '4': 2,
+  '3': 1,
+  '2': 0
+};
+
+const strengthsWithJs = {
+  ...strengthObj,
+  'T': 9,
+  '9': 8,
+  '8': 7,
+  '7': 6,
+  '6': 5,
+  '5': 4,
+  '4': 3,
+  '3': 2,
+  '2': 1,
+  'J': 0
+}
+
 export const calculateStrength = (hand: string) => {
   const counts = {};
   hand.split('').forEach(x => { counts[x] = (counts[x] || 0) + 1; });
-  console.log(counts);
-  let arr: number[] = Object.values(counts);
-  console.log(arr);
-  let twoCount = 0;
-  arr.forEach(item => { item == 2 && twoCount++ });
-  console.log(twoCount);
-  return twoCount == 2 ? 2 : Math.max(...arr) - 1;
+  const arr: number[] = Object.values(counts);
+  if (arr.every(num => num === 1)) return 0;
+  if (arr.some(num => num === 5)) return 6;
+  if (arr.some(num => num === 4)) return 5;
+  if (arr.some(num => num === 3) && arr.some(num => num === 2)) return 4;
+  if (arr.some(num => num === 3)) return 3;
+  return arr
+    .map(item => item == 2 && 1 )
+    .reduce((partial, current) => partial + current, 0);
+}
+
+export const getTotalWinnings = (arr) => {
+  const newArr = arr.sort((a,b) => {
+    const handA = a.substring(0, 5);
+    const handB = b.substring(0, 5);
+    const strengthA = calculateStrength(handA);
+    const strengthB = calculateStrength(handB);
+    if (strengthA !== strengthB) return strengthA-strengthB;
+    return compareHandsHighCard(handA, handB, strengthObj);
+  });
+  return newArr
+    .map((hand, idx) => parseInt(hand.substring(6))*(idx+1))
+    .reduce((partial, current) => partial + current, 0);
+}
+
+
+const compareHandsHighCard = (handA: string, handB: string, strengths) => {
+  const a = strengths[handA.charAt(0)];
+  const b = strengths[handB.charAt(0)];
+  if (a === b) return compareHandsHighCard(handA.substring(1), handB.substring(1), strengths);
+  return a-b;
+}
+
+export const calculateStrengthWithJs = (hand: string) => {
+  const counts = {};
+  hand.split('').forEach(x => { counts[x] = (counts[x] || 0) + 1; });
+
+  const jCount = counts['J'] ? counts['J'] : 0;
+  const arr: number[] = Object.values(counts);
+  if (arr.every(num => num === 1) && jCount) return 1;
+  if (arr.every(num => num === 1)) return 0;
+
+  if (arr.some(num => num === 5)) return 6;
+  
+  if (arr.some(num => num === 4) && jCount) return 6;
+  if (arr.some(num => num === 4)) return 5;
+
+  if (arr.some(num => num === 3) && arr.some(num => num === 2) && jCount) return 6;
+  if (arr.some(num => num === 3) && arr.some(num => num === 2)) return 4;
+
+  if (arr.some(num => num === 3) && jCount === 3) return 5;
+  if (arr.some(num => num === 3)) return 3 + (jCount ? 2 : 0);
+
+  const twoCount = arr
+    .map(item => item == 2 && 1 )
+    .reduce((partial, current) => partial + current, 0);
+  if (twoCount == 2 && jCount == 2) {
+    return 5;
+  } else if (twoCount == 2 && jCount == 1) {
+    return 4;
+  } else if (twoCount == 1 && jCount) {
+    return 3;
+  }
+  return twoCount;
+}
+
+export const getTotalWinningsWithJs = (arr) => {
+  const newArr = arr.sort((a,b) => {
+    const handA = a.substring(0, 5);
+    const handB = b.substring(0, 5);
+    const strengthA = calculateStrengthWithJs(handA);
+    const strengthB = calculateStrengthWithJs(handB);
+    if (strengthA !== strengthB) return strengthA-strengthB;
+    return compareHandsHighCard(handA, handB, strengthsWithJs);
+  });
+  return newArr
+    .map((hand, idx) => parseInt(hand.substring(6))*(idx+1))
+    .reduce((partial, current) => partial + current, 0);
 }
--- test/day7.test.ts
+++ test/day7.test.ts
@@ -1,14 +1,118 @@
 import {
-  calculateStrength
+  calculateStrength,
+  getTotalWinnings,
+  calculateStrengthWithJs,
+  getTotalWinningsWithJs
 } from "../src/day7";
+import day7Data from './data/day7Data';
 
 describe('Day 7 calculateStrength', () => {
   it('calculate strength of hands', () => {
     expect(calculateStrength('32T5K')).toBe(0);
-    expect(calculateStrength('32T3K')).toBe(1);
-    expect(calculateStrength('KTJJT')).toBe(2);
-    expect(calculateStrength('KK677')).toBe(2);
-    expect(calculateStrength('T55J5')).toBe(3);
-    expect(calculateStrength('QQQJA')).toBe(3);
+    expect(calculateStrength('QQ2TK')).toBe(1);
+    expect(calculateStrength('QQJJ3')).toBe(2);
+    expect(calculateStrength('KK776')).toBe(2);
+    expect(calculateStrength('QQQJT')).toBe(3);
+    expect(calculateStrength('QQQAA')).toBe(4);
+    expect(calculateStrength('QQQQA')).toBe(5);
+    expect(calculateStrength('QQQQQ')).toBe(6);
+  });
+});
+
+describe('Day 7 getTotalWinnings', () => {
+  it('get winnings for small input', () => {
+    const input = [
+      '32T3K 765',
+      'T55J5 684',
+      'KK677 28',
+      'KTJJT 220',
+      'QQQJA 483',
+    ]
+    expect(getTotalWinnings(input)).toBe(6440);
+  });
+  it('get winnings for straightforward set of hands', () => {
+    const input = [
+      '32T3K 1',//1
+      '42T3K 0',//0
+      'QQQQQ 6',//6
+      'T55J5 3',//3
+      'KK677 2',//2
+      'JTJJT 4',//4
+      'QQQQA 5',//5
+    ]
+    expect(getTotalWinnings(input)).toBe(112);
+  });
+  it('get winnings for hand with equal strengths', () => {
+    const input = [
+      '3223K 0',//1
+      'Q2Q2K 3',//0
+      'JJ2QQ 2',//6
+      '656J5 1',//3
+      'KK677 4',//2
+    ]
+    expect(getTotalWinnings(input)).toBe(40);
+  });
+  it('get winnings for huge data set', () => {
+    expect(getTotalWinnings(day7Data)).toBe(248105065);
+  });
+});
+
+describe('Day 7 calculateStrengthWithJs', () => {
+  it('calculate strength of hands', () => {
+    expect(calculateStrengthWithJs('32T4K')).toBe(0);
+    expect(calculateStrengthWithJs('32TJK')).toBe(1);//32TTK
+    expect(calculateStrengthWithJs('QQ2TK')).toBe(1);
+    expect(calculateStrengthWithJs('QQ443')).toBe(2);
+    expect(calculateStrengthWithJs('KK776')).toBe(2);
+    expect(calculateStrengthWithJs('JJ2TK')).toBe(3);//222TK
+    expect(calculateStrengthWithJs('J22TK')).toBe(3);//222TK
+    expect(calculateStrengthWithJs('QQQKT')).toBe(3);
+    expect(calculateStrengthWithJs('QQTTJ')).toBe(4);//QQQTT
+    expect(calculateStrengthWithJs('QQQAA')).toBe(4);
+    expect(calculateStrengthWithJs('QQQJT')).toBe(5);//QQQQT
+    expect(calculateStrengthWithJs('QQJJ3')).toBe(5);//QQQQ3
+    expect(calculateStrengthWithJs('JJJQT')).toBe(5);//QQQQT
+    expect(calculateStrengthWithJs('QQQQA')).toBe(5);
+    expect(calculateStrengthWithJs('QQQJJ')).toBe(6);//QQQQQ
+    expect(calculateStrengthWithJs('QQQQQ')).toBe(6);
+    expect(calculateStrengthWithJs('JJJJQ')).toBe(6);
+  });
+});
+
+describe('Day 7 getTotalWinningsWithJs', () => {
+  it('get winnings for small input', () => {
+    const input = [
+      '32T3K 765',
+      'T55J5 684',
+      'KK677 28',
+      'KTJJT 220',
+      'QQQJA 483',
+    ]
+    expect(getTotalWinningsWithJs(input)).toBe(5905);
+  });
+  it('get winnings for straightforward set of hands', () => {
+    const input = [
+      '32T3K 1',//1
+      '42T3K 0',//0
+      'QQQQQ 6',//6
+      'T55J5 3',//3
+      'KK677 2',//2
+      'JTJJT 4',//4
+      'QQQQA 5',//5
+    ]
+    expect(getTotalWinningsWithJs(input)).toBe(111);
+  });
+  it('get winnings for hand with equal strengths', () => {
+    const input = [
+      '3223K 0',//1
+      'Q2Q2K 3',//0
+      'JJ2QQ 2',//6
+      '656J5 1',//3
+      'KK677 4',//2
+    ]
+    expect(getTotalWinningsWithJs(input)).toBe(32);
+  });
+  it('get winnings for huge data set', () => {
+    expect(getTotalWinningsWithJs(day7Data)).toBe(249515436);
   });
 });
